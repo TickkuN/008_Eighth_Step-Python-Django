2a7e639af0a084902c16826f55e52aaa29c48a89
TickkuN <Tickkun.addEventListener@gmail.com>
2022-04-06 03:35:35 +0900

    Bug Fixed

--- config/settings.py
+++ config/settings.py
@@ -119,10 +119,12 @@ USE_TZ = True
  
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
- 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
- 
-STATICFILES_DIRS = [BASE_DIR / 'static']  # postscript
+# postscript
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+) 
  
 # TAX_RATE
 TAX_RATE = 0.1
