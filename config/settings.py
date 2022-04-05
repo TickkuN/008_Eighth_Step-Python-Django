1503968312dfe0d2356d36877ac389bbd7aaca47
TickkuN <Tickkun.addEventListener@gmail.com>
2022-04-06 03:38:25 +0900

    Bug Fix

--- config/settings.py
+++ config/settings.py
@@ -4,7 +4,7 @@ from django.contrib import messages # postscript
 import os
  
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
  
 # postscript
 env = environ.Env()
