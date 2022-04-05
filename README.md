74320fd1f43b2dd04fbb1d95c640b6c1584a5b2d
TickkuN <Tickkun.addEventListener@gmail.com>
2022-04-06 02:42:21 +0900

    First Commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# 008_Eighth_Step-Python-Django
+## 1. このリポジトリをダウンロードしてください。
+<a href="https://github.com/TickkuN/008_Eighth_Step-Python-Django/archive/refs/heads/master.zip" target="_blank"> Python-Django </a>  
+  
+## 2. 解圧してください。 
+
+
+## 3. Windows PCを使用している場合は以下のコマンドを入力してください。
+
+venv\Scripts\activate
+
+補足：Windows 10では、execution of scripts is disabled on this systemというエラーがWindows PowerShellに出ることがあります。 その場合は、Windows PowerShellを「管理者として開く」で、管理者権限で新しくウィンドウを開いてください。 そして、仮想環境を起動する前に、以下のコマンドを入力してください。
+
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+
+
+## 4. Mac PCを使用している場合は以下のコマンドを入力してください。
+
+venv/bin/activate
+
+## 5. 以下のコマンドを入力してください。
+
+python manage.py runserver
+
+## アプリを楽しんでください。
+
+http://127.0.0.1:8000/
+
+## 今回はアプリを見てもらいたいのでシークレットキーなどもアップしております。ポートフォリオなので公開しています。本番環境では秘密にしておきます。
+
+<img src="imgs/001.png">
+<img src="imgs/002.png">
+<img src="imgs/003.png">
+<img src="imgs/004.png">
+<img src="imgs/005.png">
---
+++ config/settings.py
@@ -0,0 +1,163 @@
+from pathlib import Path
+import environ  # postscript
+from django.contrib import messages # postscript
+import os
+ 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+ 
+# postscript
+env = environ.Env()
+root = environ.Path(BASE_DIR / 'secrets')
+ 
+# For production environment
+# env.read_env(root('.env.prod'))
+ 
+# For development environment
+env.read_env(root('.env.dev'))
+ 
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+ 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env.str('SECRET_KEY')
+ 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+ 
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'tickkun-market.herokuapp.com']
+ 
+ 
+# Application definition
+ 
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'base',  # postscript
+]
+ 
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+ 
+ROOT_URLCONF = 'config.urls'
+ 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],  # postscript
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'config.custom_context_processors.base',  # postscript
+            ],
+        },
+    },
+]
+ 
+WSGI_APPLICATION = 'config.wsgi.application'
+ 
+ 
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+ 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+ 
+ 
+# Password validation
+# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
+ 
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+ 
+ 
+# Internationalization
+# https://docs.djangoproject.com/en/3.1/topics/i18n/
+ 
+LANGUAGE_CODE = 'en'
+ 
+TIME_ZONE = 'Asia/Tokyo'  # changed
+ 
+USE_I18N = True
+ 
+USE_L10N = True
+ 
+USE_TZ = True
+ 
+ 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+ 
+STATIC_URL = '/static/'
+ 
+STATICFILES_DIRS = [BASE_DIR / 'static']  # postscript
+ 
+# TAX_RATE
+TAX_RATE = 0.1
+ 
+# Stripe API Key
+STRIPE_API_SECRET_KEY = env.str('STRIPE_API_SECRET_KEY')
+ 
+# Schema & Domain
+MY_URL = env.str('MY_URL')
+ 
+ 
+# Custom User Model
+AUTH_USER_MODEL = 'base.User'
+ 
+LOGIN_URL = '/login/'
+ 
+LOGIN_REDIRECT_URL = '/'
+ 
+LOGOUT_URL = '/logout/'
+ 
+LOGOUT_REDIRECT_URL = '/login/'
+ 
+ 
+# messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'rounded-0 alert alert-danger',
+    messages.WARNING: 'rounded-0 alert alert-warning',
+    messages.SUCCESS: 'rounded-0 alert alert-success',
+    messages.INFO: 'rounded-0 alert alert-info',
+    messages.DEBUG: 'rounded-0 alert alert-secondary',
+}
+ 
+ 
+# custom_context_processors
+TITLE = 'TickkuN Market'
+
+# postscript
+if os.getcwd() == '/app':
+    DEBUG = False
